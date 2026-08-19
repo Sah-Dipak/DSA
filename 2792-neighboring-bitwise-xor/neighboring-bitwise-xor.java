77f6433b346da9e3837f13a7214586d459ebd272
Dipak Kumar <dipaksah62008490@gmail.com>
2026-08-19 08:39:22 +0530

    Time: 2 ms (100.00%) | Memory: 199.3 MB (18.24%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+       
+
+        int xor = 0;
+
+        for(int ele : derived){
+            xor ^= ele;
+        }
+
+        return xor == 0;
+
+
+    }
+}
