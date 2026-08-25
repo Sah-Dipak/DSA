2e6371b4884a8db841a20cecd015a120ae28b13e
Dipak Kumar <dipaksah62008490@gmail.com>
2026-08-26 00:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (59.09%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -9,7 +9,7 @@ class Solution {
         while(left <= right){
             int mid = left + (right - left) / 2;
 
-            if(arr[mid] > arr[mid+1]){
+            if(arr[mid] >= arr[mid+1]){
                 ans = mid;
                 right = mid - 1;
             }else{
